c48421909df123e48ca8a08f158fc02418eedad1
SantoshThkr <santosh.thakur36911@gmail.com>
2020-04-15 10:12:00 +0530

    initialize github profile viewer

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>GitHub Profile Viewer</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
